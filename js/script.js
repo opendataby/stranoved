2a12801921a14a7a36d309443731b913107eac5f
ushchent <am@opendata.by>
2017-02-15 19:50:09 +0300

    Добавить карту.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var re = /област/,
 	regions = [],
 	data,
 	sort_ascending = true,
-	table = d3.select("#table").append("table"),
+	table = d3.select("#full_table").append("table"),
 	thead = table.append("thead").append("tr"),
 	tbody = table.append("tbody"),
 	table_headers,
@@ -60,7 +60,7 @@ function main() {
 						.text("")
 						.append("select")
 						.attr("id", "region-selector")
-						.on("change", function() { filter_by_region(this.value); });
+						.on("change", function() { console.log(this.value); filter_by_region(this.value); });
 		selector.selectAll("option")
 			.data(regions)
 			.enter()
@@ -92,6 +92,19 @@ function main() {
 		theaders.exit().remove();
 		redraw(data);
 		});
+		
+
+d3.select("#menu_selector").selectAll("li")
+	.on("click", function(d) {
+		var selected_item = d3.select(this).attr("class");
+		console.log(selected_item);
+		d3.selectAll("#menu_selector li").classed("active", false);
+		d3.select(this).classed("active", true);
+		d3.selectAll(".tab_content").classed("hidden", true);
+		d3.select("#" + selected_item).classed("hidden", false);
+	});
+
+d3.select("#full_table").classed("hidden", false);
 }
 
 function filter_by_region(region) {
@@ -143,3 +156,244 @@ function redraw(data) {
 
 }
 	main();
+
+
+/////////////////////////////////////
+// Карта
+
+var height = 600,
+	width = 800;
+var selected_category;
+var data_loaded = false;
+				
+var projection = d3.geoMercator().center([27.9, 53.7]).scale(4000)
+                    .translate([width / 2, height / 2]);
+var path = d3.geoPath().projection(projection);
+var color = d3.scaleQuantile()
+              .range(['#f2f0f7','#cbc9e2','#9e9ac8','#6a51a3']);
+var formatter = d3.format(",.1f");
+
+d3.json("data/rajony.geojson", function(karta) {
+	
+	// Собираем названия категорий для селектора
+
+    d3.csv("data/goroda.csv", function(goroda) {
+	data_loaded = true;
+
+var category_names = d3.map(data[0]).keys();
+	category_names.shift("region");
+	category_names.shift("subject");
+	console.log(category_names);
+	
+
+	var category_selector = d3.select("#map").append("select")
+			.attr("id", "map_selector")
+
+var svg_map = d3.select("#map")
+				.append("svg")
+				.attr("width", 800)
+				.attr("height", height);
+
+var legend = svg_map.append("g")
+            .attr("id", "legend")
+            .attr("transform", "translate(40, 50)");
+
+category_selector.on("change", function() {
+		console.log("Hello")
+					selected_category = this.value
+					redraw_map(selected_category);
+				})
+
+
+	category_selector.selectAll("option")
+		.data(category_names)
+		.enter()
+		.append("option")
+		.attr("value", function(d) { return d; })
+		.text(function(d) { return d; });
+		
+		
+
+		
+function redraw_map(selected_category) {
+	console.log(selected_category);
+
+    for (var i = 0; i < data.length; i++ ) {
+      for (var j = 0; j < karta.features.length; j++) {
+        if (data[i].subject == karta.features[j].properties.rajon) {
+          karta.features[j].properties.amount = +data[i][selected_category];
+          break;
+        }
+      }
+    };
+
+color.domain(d3.extent(data, function(d) { return +d[selected_category]; }));
+    var paths = svg_map.selectAll("path")
+      .data(karta.features)
+	
+	paths.enter()
+      .append("path")
+      .attr("d", path)
+	.attr("fill", function(d) { if (d.properties.amount) {
+                                    return color(d.properties.amount);
+                                  } else {
+                                    return "white"
+                                    }
+                                  })
+    .on("mouseover", function(d) {
+                    var xPos = d3.event.pageX + "px";
+                    var yPos = d3.event.pageY + "px";
+                    d3.select("#tooltip")
+                      .style("left", xPos)
+                      .style("top", yPos)
+                      .classed("hidden", false);
+                    d3.select("#rajon")
+                      .text(d.properties.rajon + " район" );
+                    
+                      d3.select("#amount")
+                      .text(selected_category + " : " + formatter(d.properties.amount));
+            })
+                    .on("mouseout", function(d) {
+                      d3.select("#tooltip")
+                        .classed("hidden", true)
+                      });
+                                  
+    paths.on("mouseover", function(d) {
+                    var xPos = d3.event.pageX + "px";
+                    var yPos = d3.event.pageY + "px";
+                    d3.select("#tooltip")
+                      .style("left", xPos)
+                      .style("top", yPos)
+                      .classed("hidden", false);
+                    d3.select("#rajon")
+                      .text(d.properties.rajon + " район" );
+                    
+                      d3.select("#amount")
+                      .text(selected_category + " : " + formatter(d.properties.amount));
+            })
+                    .on("mouseout", function(d) {
+                      d3.select("#tooltip")
+                        .classed("hidden", true)
+                      });
+      
+      	paths.transition()
+      	.duration(500)
+      	.attr("fill", function(d) { return color(+d.properties.amount); });
+
+      // Легенда
+
+      var legend_rects = legend.selectAll("rect")
+            .data(color.range());
+            
+        legend_rects.enter()
+            .append("rect")
+            .attr("x", 30)
+            .attr("y", function(d, i) { return i * 15; })
+            .attr("width", 10)
+            .attr("height", 10)
+            .attr("fill", function(d) { return d; })
+            .attr("stroke", "black");
+            
+      var legend_texts = legend.selectAll("text")
+            .data(color.range());
+            
+    legend_texts.enter()
+            .append("text")
+            .text(function(d) { return "<" + " " + formatter(d3.max(color.invertExtent(d), function(d) { return d; })); })
+            .attr("x", 45)
+            .attr("y", function(d, i) { return (i * 15) + 10; });
+  
+  legend_texts.transition()
+	.duration(500)
+	.text(function(d) { return "<" + " " + formatter(d3.max(color.invertExtent(d), function(d) { return d; })); })
+  
+        var cities = svg_map.selectAll("circle")
+                          .data(goroda);
+                    
+                    cities.enter()
+                       .append("circle")
+                       .attr("class", "city")
+                       .attr("cx", function(d) {
+                         return projection([d.lon, d.lat])[0];
+                       })
+                       .attr("cy", function(d) {
+                         return projection([d.lon, d.lat])[1];
+                       })
+                       .attr("r", function(d) {
+                         if (d.city == "г. Минск") {
+                           return 7;
+                         } else {
+                           return 5;
+                         };})                    
+                    .attr("fill", function(d) { 
+                                var colorCircle;
+                                for (var q = 0; q < data.length; q++) {
+                                  if (d.city == data[q].subject) {
+                                      colorCircle = color(data[q][selected_category]);
+                                      d.amount = data[q][selected_category];
+                                    } else {
+                                      continue;
+                                  };
+                                };
+                                return colorCircle;
+                                })
+                    .on("mouseover", function(d) {
+                    var xPos = d3.event.pageX + "px";
+                    var yPos = d3.event.pageY + "px";
+                    d3.select("#tooltip")
+                      .style("left", xPos)
+                      .style("top", yPos)
+                      .classed("hidden", false);
+                    d3.select("#rajon")
+                      .text(d.city );
+                    
+                      d3.select("#amount")
+                      .text(selected_category + " : " + formatter(d.amount));
+            })
+                    .on("mouseout", function(d) {
+                      d3.select("#tooltip")
+                        .classed("hidden", true)
+                      });
+                      
+                      
+                      
+        cities.transition()
+			.duration(500)
+			.attr("fill", function(d) { 
+                                var colorCircle;
+                                for (var q = 0; q < data.length; q++) {
+                                  if (d.city == data[q].subject) {
+                                      colorCircle = color(data[q][selected_category]);
+                                      d.amount = data[q][selected_category];
+                                    } else {
+                                      continue;
+                                  };
+                                };
+                                return colorCircle;
+                                });
+                
+        cities.on("mouseover", function(d) {
+                    var xPos = d3.event.pageX + "px";
+                    var yPos = d3.event.pageY + "px";
+                    d3.select("#tooltip")
+                      .style("left", xPos)
+                      .style("top", yPos)
+                      .classed("hidden", false);
+                    d3.select("#rajon")
+                      .text(d.city );
+                    
+                      d3.select("#amount")
+                      .text(selected_category + " : " + formatter(d.amount));
+            })
+                    .on("mouseout", function(d) {
+                      d3.select("#tooltip")
+                        .classed("hidden", true)
+                      });
+                      
+	}
+
+redraw_map(category_names[0])
+
+});
+
+});
